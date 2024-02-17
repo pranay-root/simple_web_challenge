e1306153714ea27efb9d2ec74aa1bf1c4f315282
Iruvuri Pranay <pranaygoud53@gmail.com>
2024-02-17 14:45:45 +0530

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,9 +1,11 @@
 function checkFlag() {
-    var userFlag = document.getElementById('flag').value;
+    var userFlag = document.getElementById('flag').value.trim(); // Trim to remove leading/trailing whitespaces
     var correctFlag = 'CTF' + String.fromCharCode(123) + 'y0u' + String.fromCharCode(95) + 'W0n' + String.fromCharCode(95) + 'The' + String.fromCharCode(95) + 'Cha11enge' + String.fromCharCode(125);
 
-    if (userFlag === correctFlag) {
-        document.getElementById('resultMessage').innerHTML = '<span style="color: #2ecc71;">Congratulations! It is the right flag.</span>';
+    var secretFlag = 'SCT{iam_secret_but_1_am_not_flag}';
+
+    if (userFlag === secretFlag) {
+        document.getElementById('resultMessage').innerHTML = '<span style="color: #2ecc71;">Congratulations! It is the right flag: ' + correctFlag + '</span>';
     } else {
         document.getElementById('resultMessage').innerHTML = '<span style="color: #e74c3c;">Try again. It is not the right flag.</span>';
     }
